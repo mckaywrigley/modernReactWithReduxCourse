d334b43015202be05b7c92412780668bbec71f87
Mckay <mckaywrigley@gmail.com>
2019-03-17 14:57:18 -0700

    adds Comment component

---
+++ blogApp/blog/src/Components/Comment.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Comment extends Component {
+    constructor() {
+        super();
+        this.state = {
+
+        };
+    };
+
+    render() {
+        return (
+            <>
+                <h3>Name</h3>
+                <p>Comment</p>
+            </>
+        );
+    };
+};
+
+export default Comment;
