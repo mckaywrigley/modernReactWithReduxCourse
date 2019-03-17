ee805e27efb76ac50cad683a353d7392e8a4244d
Mckay <mckaywrigley@gmail.com>
2019-03-17 16:30:21 -0700

    adds SeasonView

--- seasonsApp/seasons/src/App.js
+++ seasonsApp/seasons/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import SeasonView from './SeasonView';
 
 class App extends Component {
   render() {
     return (
       <>
-        
+        <SeasonView />
       </>
     );
   }
---
+++ seasonsApp/seasons/src/SeasonView.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class SeasonView extends Component {
+    constructor() {
+        super();
+        this.state = {
+
+        };
+    };
+
+    render() {
+        return (
+            <div className="seasonView">
+                <h1>Seasons</h1>
+            </div>
+        );
+    };
+};
+
+export default SeasonView;
