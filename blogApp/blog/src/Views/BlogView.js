0eea44111b6965ff4073dce78db76a604e09878c
Mckay <mckaywrigley@gmail.com>
2019-03-17 14:49:27 -0700

    adds BlogView

---
+++ blogApp/blog/src/Views/BlogView.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class BlogView extends Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <>
+                <PostList />
+            </>
+        );
+    };
+}
+
+export default BlogView;
