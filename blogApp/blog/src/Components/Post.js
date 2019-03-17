67921ed0b0dfe50ebb0b8bc1d0528c977a4a15e1
Mckay <mckaywrigley@gmail.com>
2019-03-17 15:30:29 -0700

    styles Post

--- blogApp/blog/src/Components/Post.js
+++ blogApp/blog/src/Components/Post.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Comment from './Comment';
+import './Post.scss';
 
 class Post extends Component {
     constructor() {
@@ -11,14 +12,14 @@ class Post extends Component {
 
     render() {
         return (
-            <>
+            <div className="post">
                 <h2>{this.props.title}</h2>
-                <h3>{this.props.name}</h3>
+                <h3>By {this.props.name}</h3>
                 <p>{this.props.text}</p>
                 <Comment name='David' text='Great post!'/>
                 <Comment name='Michael' text='Very insightful!'/>
                 <Comment name='Joe' text='Wonderful read!'/>
-            </>
+            </div>
         );
     };
 }
