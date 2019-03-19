ea15841ca75fbd2f8489e08098d40f4581aa8efe
Mckay <mckaywrigley@gmail.com>
2019-03-19 14:19:55 -0700

    adds SearchBar component

--- videoApp/video-search/src/App.js
+++ videoApp/video-search/src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import SearchBar from './Components/SearchBar';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
+        <h1>Video Search App</h1>
+        <SearchBar />
       </div>
     );
   }
---
+++ videoApp/video-search/src/Components/SearchBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+
+        };
+    };
+
+    render() {
+        return (
+            <div className="searchBar">
+                <h2>Search Bar</h2>
+            </div>
+        );
+    };
+};
+
+export default SearchBar;
