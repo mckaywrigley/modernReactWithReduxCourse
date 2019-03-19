d996ef1bbed689a3e7073d7b809bf64a8775242c
Mckay <mckaywrigley@gmail.com>
2019-03-19 16:14:10 -0700

    wires up axios get request to youtube that succesfully gets data

--- videoApp/video-search/src/App.js
+++ videoApp/video-search/src/App.js
@@ -3,13 +3,25 @@ import youtube from './apis/youtube';
 import SearchBar from './Components/SearchBar';
 
 class App extends Component {
+
+  onTermSubmit = async term => {
+    const res = await youtube.get('/search', {
+      params: {
+        q: term
+      }
+    });
+    console.log(res.data);
+  };
+
   render() {
     return (
       <div className="app ui container">
-        <SearchBar />
+        <SearchBar 
+          onFormSubmit={this.onTermSubmit}
+        />
       </div>
     );
-  }
-}
+  };
+};
 
 export default App;
