b3908d7cf216f0aa706b934d58c42618fd509a1b
Mckay <mckaywrigley@gmail.com>
2019-03-19 09:48:36 -0700

    refactors promise syntax to asynx/await

--- imageSearchApp/image-search/src/App.js
+++ imageSearchApp/image-search/src/App.js
@@ -4,13 +4,15 @@ import SearchBar from './Components/SearchBar';
 
 
 class App extends Component {
-  onSearchSubmit = term => {
-    axios.get('https://api.unsplash.com/search/photos', {
-      params: { query: term },
-      headers: {
-        Authorization: 'Client-ID 40bdaf803000b0cb28dcbc55ef89093104e9d29854971bb8be382053a0a79fe2'
-    }
-    });
+  async onSearchSubmit(term) {
+    const res = await axios
+      .get('https://api.unsplash.com/search/photos', {
+        params: { query: term },
+        headers: {
+          Authorization: 'Client-ID 40bdaf803000b0cb28dcbc55ef89093104e9d29854971bb8be382053a0a79fe2'
+      }
+      });
+    console.log(res.data.results);
   };
 
   render() {
