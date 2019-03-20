659854e06ff0f3d39dc6da6bd651118e1aee8221
Mckay <mckaywrigley@gmail.com>
2019-03-19 20:05:17 -0700

    adds iframe with video player for VideoDetail

--- videoApp/video-search/src/Components/VideoDetail.js
+++ videoApp/video-search/src/Components/VideoDetail.js
@@ -11,6 +11,11 @@ const VideoDetail = ({ video }) => {
 
     return (
         <div>
+            <div className="ui embed">
+                <iframe 
+                    src={`https://www.youtube.com/embed/${video.id.videoId}`}
+                />
+            </div>
             <div className="ui segment">
                 <h4 className="ui header">{video.snippet.title}</h4>
                 <p>{video.snippet.description}</p>
