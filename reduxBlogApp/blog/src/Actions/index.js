029d0c20654c3430d03d42e6771d5f6d17f19861
Mckay <mckaywrigley@gmail.com>
2019-03-21 14:40:08 -0700

    renders list of posts

--- reduxBlogApp/blog/src/Actions/index.js
+++ reduxBlogApp/blog/src/Actions/index.js
@@ -3,5 +3,5 @@ import jsonPlaceholder from '..//APIs/jsonPlaceholder';
 export const fetchPosts = () =>  async dispatch => {
     const res = await jsonPlaceholder.get('/posts');
 
-    dispatch({ type: 'FETCH_POSTS', payload: res });
+    dispatch({ type: 'FETCH_POSTS', payload: res.data });
 };
