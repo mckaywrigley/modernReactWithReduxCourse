f303f5e343406c5aba5152d873513736c011b673
Mckay <mckaywrigley@gmail.com>
2019-03-22 20:59:36 -0700

    deploys videoApp successfully

--- videoApp/video-search/src/Components/VideoDetail.js
+++ videoApp/video-search/src/Components/VideoDetail.js
@@ -4,7 +4,7 @@ const VideoDetail = ({ video }) => {
     if(!video) {
         return (
             <div>
-                Loading...
+                Please search above :)
             </div>
         );
     }
