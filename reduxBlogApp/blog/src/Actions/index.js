de01b7bceb07d37347b1a9786f79038def459481
Mckay <mckaywrigley@gmail.com>
2019-03-23 13:25:29 -0700

    implements lodash memoize into fetchUser action creator

--- reduxBlogApp/blog/src/Actions/index.js
+++ reduxBlogApp/blog/src/Actions/index.js
@@ -1,4 +1,5 @@
 import jsonPlaceholder from '../APIs/jsonPlaceholder';
+import _ from 'lodash';
 
 export const fetchPosts = () =>  async dispatch => {
     const res = await jsonPlaceholder.get('/posts');
@@ -6,8 +7,12 @@ export const fetchPosts = () =>  async dispatch => {
     dispatch({ type: 'FETCH_POSTS', payload: res.data });
 };
 
-export const fetchUser = (id) => async dispatch => {
+export const fetchUser = id => dispatch => {
+    _fetchUser(id, dispatch);
+};
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
     const res = await jsonPlaceholder.get(`/users/${id}`);
 
     dispatch({ type: 'FETCH_USER', payload: res.data });
-}
+});
