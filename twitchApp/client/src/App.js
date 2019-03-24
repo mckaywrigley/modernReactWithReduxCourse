b3ffef569aeeafd7e198a6b7780804616938b86d
Mckay <mckaywrigley@gmail.com>
2019-03-23 17:34:19 -0700

    adds React Router

--- twitchApp/client/src/App.js
+++ twitchApp/client/src/App.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const PageOne = () => {
+    return (
+        <div>
+            Page One
+            <Link to="/2">Page Two</Link>
+        </div>
+    );
+}
+
+const PageTwo = () => {
+    return (
+        <div>
+            Page Two
+            <Link to="/">Page One</Link>
+        </div>
+    );
+}
 
 const App = () => {
     return (
         <div className="App">
-            <h1>App</h1>
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={PageOne} />
+                    <Route path="/2" component={PageTwo} />
+                </div>
+            </BrowserRouter>
         </div>
     );
 }
