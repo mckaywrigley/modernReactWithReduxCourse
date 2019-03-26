525104dbb1f64355c69fa3b0443c295d8d3d54f3
Mckay <mckaywrigley@gmail.com>
2019-03-26 14:41:04 -0700

    takes user back to streamList after creating stream

--- twitchApp/client/src/Components/Streams/StreamList.js
+++ twitchApp/client/src/Components/Streams/StreamList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchStreams } from '../../Actions';
 
 class StreamList extends React.Component {
@@ -23,10 +24,12 @@ class StreamList extends React.Component {
     };
 
     renderCreate() {
-        if (currentUserId) {
+        if (this.props.isSignedIn) {
             return (
-                <div>
-                    Create
+                <div style={{ textAlign: 'right' }}>
+                    <Link to="/streams/new" className="ui button primary">
+                        Create Stream
+                    </Link>
                 </div>
             );
         }
@@ -50,12 +53,12 @@ class StreamList extends React.Component {
     };
     
     render() {
-        console.log(this.props.streams);
         return (
             <div>
                 <h2>Streams</h2>
                 <div className="ui celled list">
                     {this.renderList()}
+                    {this.renderCreate()}
                 </div>
             </div>
         );
@@ -65,7 +68,8 @@ class StreamList extends React.Component {
 const mapStateToProps = state => {
     return { 
         streams: Object.values(state.streams),
-        currentUserId: state.auth.userId
+        currentUserId: state.auth.userId,
+        isSignedIn: state.auth.isSignedIn
     }
 };
 
