cc33c35b2ce38f9263e9aaea487176cbddbd0796
Mckay <mckaywrigley@gmail.com>
2019-03-26 14:11:47 -0700

    renders edit and delete buttons, not yet working

--- twitchApp/client/src/Components/Streams/StreamList.js
+++ twitchApp/client/src/Components/Streams/StreamList.js
@@ -1,11 +1,75 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../Actions';
 
-const StreamList = () => {
-    return (
-        <div>
-            StreamList
-        </div>
-    );
+class StreamList extends React.Component {
+    componentDidMount() {
+        this.props.fetchStreams();
+    }
+
+    renderAdmin(stream) {
+        if (stream.userId === this.props.currentUserId) {
+            return (
+                <div className="right floated content">
+                    <button className="ui button primary">
+                        Edit
+                    </button>
+                    <button className="ui button negative">
+                        Delete
+                    </button>
+                </div>
+            )
+        }
+    };
+
+    renderCreate() {
+        if (currentUserId) {
+            return (
+                <div>
+                    Create
+                </div>
+            );
+        }
+    };
+
+    renderList() {
+        return this.props.streams.map(stream => {
+            return (
+                <div className="item" key={stream.id}>
+                    {this.renderAdmin(stream)}
+                    <i className="large middle aligned icon camera" />
+                    <div className="content">
+                        {stream.title}
+                        <div className="description">
+                            {stream.description}
+                        </div>
+                    </div>
+                </div>
+            )
+        });
+    };
+    
+    render() {
+        console.log(this.props.streams);
+        return (
+            <div>
+                <h2>Streams</h2>
+                <div className="ui celled list">
+                    {this.renderList()}
+                </div>
+            </div>
+        );
+    }
 }
 
-export default StreamList;
+const mapStateToProps = state => {
+    return { 
+        streams: Object.values(state.streams),
+        currentUserId: state.auth.userId
+    }
+};
+
+export default connect(
+    mapStateToProps, 
+    { fetchStreams }
+)(StreamList);
